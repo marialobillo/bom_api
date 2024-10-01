40038c0ba21201243f5bf252ff9fb334c105a3cd
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-10-01 18:42:41 +0200

    feat: add database postgres connection

---
+++ infrastructure/db/postgres.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func Connect() {
+	var err error
+	DB, err = sql.Open("postgres", "user=postgres password=postgres dbname=bom_api sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = DB.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connected to database successfully")
+}
