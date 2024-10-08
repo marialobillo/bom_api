6ee8b800abcedd677e13271ed85240b14de8770d
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-10-08 16:03:02 +0200

    fix: remove the id params from part repository the postgres db will generate uuid

--- internal/repository/part_repository.go
+++ internal/repository/part_repository.go
@@ -9,8 +9,8 @@ import (
 )
 
 func CreatePart(part *entities.Part) error {
-	query := "INSERT INTO parts (id, name, supplier_id, price, available, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id"
-	err := db.DB.QueryRow(query, part.ID, part.Name, part.Supplier_id, part.Price, part.Available, part.Created_at, part.Updated_at).Scan(&part.ID)
+	query := "INSERT INTO parts ( name, supplier_id, price, available, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6 ) RETURNING id"
+	err := db.DB.QueryRow(query, part.Name, part.Supplier_id, part.Price, part.Available, part.Created_at, part.Updated_at).Scan(&part.ID)
 	if err != nil {
 		log.Println("Error creating part: ", err)
 		return fmt.Errorf("failed to create part: %w", err)
