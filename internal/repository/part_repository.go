3b0a6492accdf0f5a9858a8488b0b5a0f0c2c950
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-10-08 17:00:42 +0200

    fix: repository part changes some attibutes and names to fit

--- internal/repository/part_repository.go
+++ internal/repository/part_repository.go
@@ -1,33 +1,31 @@
 package repository
 
 import (
-	"fmt"
-	"log"
-
 	"github.com/marialobillo/bom_api/infrastructure/db"
 	"github.com/marialobillo/bom_api/internal/entities"
+	"log"
 )
 
 type PartRepository interface {
-    CreatePart(part *entities.Part) error
+	CreatePart(part *entities.Part) error
 }
 
 type PartRepo struct {
-	DB *db.Database
+	db *db.Database
 }
 
-func NewPartRepository(db *db.Database) PartRepository {
+func NewPartRepository(database *db.Database) PartRepository {
 	return &PartRepo{
-		db: db,
+		db: database,
 	}
 }
 
 func (r *PartRepo) CreatePart(part *entities.Part) error {
-    query := "INSERT INTO parts (name, supplier_id, price, available, description, quantity, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id"
-    err := r.db.DB.QueryRow(query, part.Name, part.Supplier_id, part.Price, part.Available, part.Description, part.Quantity, part.Created_at, part.Updated_at).Scan(&part.ID)
-    if err != nil {
-        log.Println(err)
-        return err
-    }
-    return nil
+	query := "INSERT INTO parts (name, supplier_id, price, available, description, quantity, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id"
+	err := r.db.DB.QueryRow(query, part.Name, part.Supplier_id, part.Price, part.Available, part.Description, part.Quantity, part.Created_at, part.Updated_at).Scan(&part.ID)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
 }
