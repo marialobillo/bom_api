e2ca8a2dffc4db6e2320b84e1f76f44eb396c68c
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-10-08 17:13:11 +0200

    fix: upate the part handler for create part

--- internal/handler/part_handler.go
+++ internal/handler/part_handler.go
@@ -1,15 +1,34 @@
 package handler
 
 import (
-	"github.com/marialobillo/bom_api/internal/service"
+	"net/http"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/marialobillo/bom_api/internal/entities"
+	"github.com/marialobillo/bom_api/internal/service"
 )
 
 type PartHandler struct {
-	partService *service.Createpart
+	partService *service.PartService
 }
 
-func NewPartHandler(partService *service.CreatePart) *PartHandler {
+func NewPartHandler(partService *service.PartService) *PartHandler {
 	return &PartHandler{partService: partService}
 }
 
+func (h *PartHandler) CreatePart(c * fiber.Ctx) error {
+	var part entities.Part 
+
+	if err := c.BodyParser(&part); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	if err := h.partService.CreatePart(&part); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+	return c.Status(http.StatusCreated).JSON(part)
+}
