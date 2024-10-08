13830ae110d138aff9e01fab49b426cf74d24fe2
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-10-08 17:02:27 +0200

    fix: using part repository on main now

--- cmd/main.go
+++ cmd/main.go
@@ -7,17 +7,17 @@ import (
 	"github.com/marialobillo/bom_api/infrastructure/db"
 	"github.com/marialobillo/bom_api/internal/entities"
 	"github.com/marialobillo/bom_api/internal/handler"
+	"github.com/marialobillo/bom_api/internal/repository"
 	"github.com/marialobillo/bom_api/internal/service"
 )
 
 func main() {
-	
 	// Initialize database connection
-	dbConn := db.NewPostgresConnection()
-	defer dbConn.Close()
-	
+	database := db.NewPostgresConnection()
+	defer database.Close()
+
 	// Setup  repository and service
-	partRepo := repository.NewPartRepository()
+	partRepo := repository.NewPartRepository(database)
 	partService := service.NewPartService(partRepo)
 
 	// Setup handler 
@@ -26,7 +26,7 @@ func main() {
 	app := fiber.New()
 	apiv1 := app.Group("/api/v1")
 
-	
+
 	routes.SetupRoutes(apiv1, partHandler)
 
 	if err := app.Listen(":3300"); err != nil {
