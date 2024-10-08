82f0ad770c831668b8bbfaec7ed94878f95faf17
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2024-10-08 12:51:56 +0200

    feat: add more explicit message to the error on creating part

--- internal/repository/part_repository.go
+++ internal/repository/part_repository.go
@@ -1,18 +1,19 @@
 package repository
 
 import (
+	"fmt"
 	"log"
 
-	"github.com/marialobillo/bom_api/internal/entities"
 	"github.com/marialobillo/bom_api/infrastructure/db"
+	"github.com/marialobillo/bom_api/internal/entities"
 )
 
 func CreatePart(part *entities.Part) error {
 	query := "INSERT INTO parts (id, name, supplier_id, price, available, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id"
 	err := db.DB.QueryRow(query, part.ID, part.Name, part.Supplier_id, part.Price, part.Available, part.Created_at, part.Updated_at).Scan(&part.ID)
 	if err != nil {
-		log.Println(err)
-		return err
+		log.Println("Error creating part: ", err)
+		return fmt.Errorf("failed to create part: %w", err)
 	}
 	return nil
 }
